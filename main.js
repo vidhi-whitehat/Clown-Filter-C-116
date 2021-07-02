d40fd69e96ff149243b74ed2fe1c39181dde9bbc
vidhi-whitehat <76781620+vidhi-whitehat@users.noreply.github.com>
2021-07-02 20:13:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+noseX=0;
+noseY=0;
+
+function preload() {
+    clown_nose = loadImage('https://i.postimg.cc/Dmkb7dkQ/Clown-nose-large.png')
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initilized')
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x
+        noseY = results[0].pose.nose.y
+        console.log("nose x " + noseX);
+        console.log("nose y " + noseY);
+    }
+}
+function draw() {
+    image(video, 0, 0, 300, 300);
+    fill(255, 0, 0);
+    stroke(255, 0, 0);
+    circle(noseX, noseY, 20);
+    image(clown_nose, noseX, noseY, 30, 30);
+}
+
+function take_snapshot() {
+    save ('MyFilterImage.png')
+}
